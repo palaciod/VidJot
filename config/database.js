a01d839ed73be56189781892551653709b7b9886
Dan <dpalacio@buffalo.edu>
2019-10-31 01:20:03 -0400

    third commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://dpalacio:palacio@vidjot-prod-59z5o.mongodb.net/test?retryWrites=true&w=majority'
+    module.exports = {mongoURI: 'mongodb+srv://dpalacio:palacio@vidjot-prod-59z5o.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'Secret'
     }
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
