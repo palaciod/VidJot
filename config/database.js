adde83c6bc73cd0b2d44e7e921f8e427b5b441f0
Dan <dpalacio@buffalo.edu>
2019-10-31 01:01:27 -0400

    second commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://CHmongodb+srv://dpalacio:<password>@vidjot-prod-59z5o.mongodb.net/test?retryWrites=true&w=majorityANGEME'}
+    module.exports = {mongoURI: 'mongodb+srv://dpalacio:<NetFlix05>@vidjot-prod-59z5o.mongodb.net/test?retryWrites=true&w=majority'}
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
   }
