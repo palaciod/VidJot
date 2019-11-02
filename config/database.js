f558d18f9217b9bfcde323305c52d5d495281fd0
Dan <dpalacio@buffalo.edu>
2019-11-02 16:37:10 -0400

    password change

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {mongoURI: "mongodb+srv://dpalacio:<password>@vidjot1234-pj2bq.mongodb.net/test?retryWrites=true&w=majority",
     
+    
     }
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
