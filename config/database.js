fed8fe49683b93331f3b29a64c3e8c150e134b76
Dan <dpalacio@buffalo.edu>
2019-11-02 14:45:05 -0400

    pre-hero

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://dpalacio:palacio@vidjot-prod-59z5o.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'Secret'
+    module.exports = {mongoURI: "mongodb+srv://dpalacio:<password>@vidjot1234-pj2bq.mongodb.net/test?retryWrites=true&w=majority",
+    
     }
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
